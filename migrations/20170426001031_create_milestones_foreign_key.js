a5e3419ef9526b10d7377b97963a1cd3290b9159
Rohit Moza <moza.rohit@gmail.com>
2017-04-26 00:33:27 +0000

    Migrations worked out

---
+++ migrations/20170426001031_create_milestones_foreign_key.js
@@ -0,0 +1,22 @@
+
+exports.up = function(knex, Promise) {
+return Promise.all([
+  knex.schema.table('milestones', function (table) {
+    table.integer('famous_people_id').unsigned()
+    table.foreign('famous_people_id').references('famous_people.id')
+  })
+]);
+
+};
+
+exports.down = function(knex, Promise) {
+   return Promise.all([
+
+    knex.schema.table('milestones', function(t) {
+               t.dropForeign('famous_people_id')
+                t.dropColumn('famous_people_id')
+              })
+
+  ]);
+
+};
